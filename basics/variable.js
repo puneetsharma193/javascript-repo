23af2b35537f05f3f8f68b4c34caee74bcde8680
puneetsharma193 <143215099+puneetsharma193@users.noreply.github.com>
2024-07-20 05:29:34 +0000

    all commit

---
+++ basics/variable.js
@@ -0,0 +1,13 @@
+const userId = 1080
+let userEmail = "sharmajii@gmail.com"
+var userPassword = "123456"
+myCity = "kanpur"
+let myState;
+
+// userId = 720 not allowed
+
+userEmail = "sharma@gmail.com"
+userPassword = "2323233"
+myCity = "Farrukhabad"
+
+console.table([userEmail, userId, userPassword, myCity, myState])
