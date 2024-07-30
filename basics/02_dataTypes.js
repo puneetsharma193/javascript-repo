d8280c044d69bbc2d28a4861077121d6e9605065
puneetsharma193 <143215099+puneetsharma193@users.noreply.github.com>
2024-07-30 14:23:23 +0000

    datatypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version
+
+//  alert(3 + 3) // we are using node not browser
+
+console.log(3 + 3)
+
+console.log("sharma")//code readability should high 
+
+let name = "puneet"//string
+let age = 22// numeric
+let isSignedIn = true //boolean
+
+// number => 2 to power 53
+// bigint
+// string => true/false
+// null => standalone value
+// undefined =>
+// symbol => to find the uniqueness
+
+// object
+
+console.log(typeof null) // null is objectType 
+console.log(typeof undefined) // undefined
+console.log(typeof isSignedIn)// boolean
