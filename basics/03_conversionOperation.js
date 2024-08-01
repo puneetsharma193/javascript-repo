b3ffc1b5ae906f0bffc0afec5f2497aa791dea7a
puneetsharma193 <143215099+puneetsharma193@users.noreply.github.com>
2024-08-01 14:19:22 +0000

    new changes

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score;
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let value = String(score)
+console.log(typeof value)
+console.log(value)
+
+//In boolean 
+//"" => false
+// 1,2.... => true
+// any non empty string => true
+// 0 => false
+
+//In Number
+// null => 0
+// "abc" => NaN 
+// true => 1
+
+// In String
+// 1 => "1"
+// true => "true"
+// null => null
+
