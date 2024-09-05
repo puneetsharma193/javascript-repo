f13a696778cd49f8ebd2e0030daf2d80fd2bfeba
puneetsharma193 <143215099+puneetsharma193@users.noreply.github.com>
2024-09-05 18:08:05 +0000

    new changes

---
+++ basics/07_dates.js
@@ -0,0 +1,27 @@
+// ---Dates-----
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.getHours());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date("01-14-2024")
+console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime());
+
+
+
+
+
+
+
+
