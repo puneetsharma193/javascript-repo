0267a3b1489744c1776b280468c3caffafbf15bf
puneetsharma193 <143215099+puneetsharma193@users.noreply.github.com>
2024-09-09 15:43:57 +0000

    new changes

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,27 @@
+const arr1 = [1,2,3,4,5,6,7]
+const arr2 = ["abc","def","ghi"]
+
+// arr1.push(arr2)
+// console.log(arr1);
+
+myArr = arr1.concat(arr2)
+// console.log(myArr);
+
+const allArr = [...arr1, ...arr2,...myArr]//  Spread operator
+// console.log(allArr);
+
+const arr3 = [10,20,[30,40,50],60,70,[80,90,[95,100,[101,102,103,104,105]]]]
+const myArr2 = arr3.flat(3)
+
+console.log(myArr2);
+
+console.log(Array.isArray("sharmajii"));
+console.log(Array.from("sharmajii"));
+console.log(Array.from({name:"sharma"}));// we should declare what will be converted i.e. key or value
+
+let a1 = 100 , a2 = 200, a3 = 300;
+
+console.log(Array.of(a1,a2,a3));
+
+
+
--- basics/07_dates.js
+++ basics/07_dates.js
@@ -15,8 +15,14 @@ console.log(myCreatedDate.toLocaleString());
 
 
 let myTimeStamp = Date.now()
-console.log(myTimeStamp)
+console.log(myTimeStamp);
 console.log(myCreatedDate.getTime());
+console.log(Math.round(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
 
 
 
