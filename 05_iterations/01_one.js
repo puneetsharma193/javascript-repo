a26ea2fbf54fcbcc417519018a7929b271cb3a0a
puneetsharma193 <143215099+puneetsharma193@users.noreply.github.com>
2024-09-23 18:13:10 +0000

    new changes

---
+++ 05_iterations/01_one.js
@@ -0,0 +1,11 @@
+// For
+
+let array = [12, 233, 455];
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  if (i == 1) {
+    console.log("puneet");
+  }
+  console.log(element);
+}
